d1d9d5e51c4d10d127d788a888b8d48ed380fd9c
SzilardKosa <kszilard777@gmail.com>
2020-04-01 01:00:24 +0200

    auth guard added

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { AuthGuard } from '../services/auth.guard';
 
 
 const routes: Routes = [
   { path: 'recipes', component: RecipeListComponent },
   { path: 'recipe-detail/:id', component: RecipeDetailComponent },
-  { path: 'add-recipe', component: AddRecipeComponent },
+  { path: 'add-recipe', component: AddRecipeComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -4,11 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from '../services/auth.guard';
 
 const routes: Routes = [
-  { path: 'calculator', component: CalculatorComponent },
-  { path: 'favorites', component: FavoritesComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'calculator', component: CalculatorComponent, canActivate: [AuthGuard] },
+  { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard]  },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]  },
 ];
 
 @NgModule({
